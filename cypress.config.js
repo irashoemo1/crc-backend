871819084313c7004616edc80bd0c4cb2ef43717
Ira Shoemo <irashoemo1@gmail.com>
2023-03-09 13:11:02 -0500

    added cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    projectId: "qxo2p9",
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
