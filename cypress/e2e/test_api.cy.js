2d9e8a041477f047728970ec23bf4098d4aae424
Ira Shoemo <irashoemo1@gmail.com>
2023-03-09 14:28:06 -0500

    change resource for lambda permission and applied to terraform

--- cypress/e2e/test_api.cy.js
+++ cypress/e2e/test_api.cy.js
@@ -1,6 +1,6 @@
 describe('Test visit count api', () => {
     it('fetches visitorCounter', () => {
-        cy.request('GET', `https://aqz2aoirg0.execute-api.us-east-2.amazonaws.com/dev/visitors/1`)
+        cy.request('GET', `/1`)
             .then((resp) => {
                 const data = resp.body;
 
